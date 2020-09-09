07aaf4c7b690ca717f7f48cc36eef549d5e8edd9
Romero Medeiros <romeromedeiros5@gmail.com>
2020-09-09 00:56:09 -0300

    Adds task service.

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Task } from '../models/task.model';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private baseUrl = `${environment.apiURL}/tasks`;
+
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.baseUrl}`).pipe(
+      catchError((err: any) => throwError(err)),
+    );
+  }
+}
