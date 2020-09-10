221fd99525f5844d3dd6f36786de6ca265db9990
Romero Medeiros <romeromedeiros5@gmail.com>
2020-09-10 20:59:14 -0300

    Implements 'done' update action.

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -9,22 +9,44 @@ import { Task } from '../models/task.model';
 })
 export class TaskComponent implements OnInit {
   tasks: Task[] = [];
+  selectedTask: Task;
 
   constructor(private service: TaskService) { }
 
   ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
     this.service.findAll().subscribe(
       (responseData: any) => {
         console.log(responseData);
         this.tasks = responseData;
       },
       (responseError) => {
-        console.error(responseError);
+        alert('An error has occurred while trying to load the tasks');
+        console.error('An error has occurred while trying to load the tasks', responseError);
       },
       () => {
-        console.debug('>> Task.findAll().complete');
+        console.debug('>> This is just an example of the "complete" step of an observable subscribe.');
       }
     )
   }
 
+  handleSelect(task: Task): void {
+    this.selectedTask = task;
+  }
+
+  handleDoneChange(task, newDoneValue): void {
+    this.service.update({ ...task, done: newDoneValue }).subscribe(
+      (responseSuccess) => {},
+      (responseError) => {
+        alert('An error has occurred while trying to change the task status');
+        console.error('An error has occurred while trying to change the task status', responseError);
+      },
+      () => {
+        this.loadTasks();
+      }
+    );
+  }
 }
